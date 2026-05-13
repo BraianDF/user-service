bb9ac68b288f833b573f6ba625439962a1c43063
BraianDF <geraldo.bryan.vbnb@gmail.com>
2026-05-13 11:06:08 -0300

    feat(user): adiciona controller de usuários

---
+++ src/main/java/br/com/user_service/controller/UsuarioController.java
@@ -0,0 +1,42 @@
+package br.com.user_service.controller;
+
+import br.com.user_service.dto.response.UsuarioDetalhesResponseDTO;
+import br.com.user_service.dto.response.UsuarioListarResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users")
+public class UsuarioController {
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping
+    public ResponseEntity<Page<UsuarioListarResponseDTO>> listar(@PageableDefault(page = 0, size = 10, sort = "email", direction = Sort.Direction.ASC) Pageable pageable, @RequestParam(defaultValue = "") String email) {
+        Page<UsuarioListarResponseDTO> response = usuarioService.listar(pageable, email);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/{idUsuario}")
+    public ResponseEntity<UsuarioDetalhesResponseDTO> buscarPorId(@PathVariable UUID idUsuario) {
+        UsuarioDetalhesResponseDTO response = usuarioService.buscarPorId(idUsuario);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UsuarioDetalhesResponseDTO> buscarPorUsuario(Authentication authentication) {
+        UsuarioDetalhesResponseDTO response = usuarioService.buscarPorUsuario(authentication);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+
+}
