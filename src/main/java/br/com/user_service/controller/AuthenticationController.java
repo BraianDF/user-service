474ab5f652151d033b4891a7a0b5bf2dd7400c0a
BraianDF <geraldo.bryan.vbnb@gmail.com>
2026-05-13 10:29:49 -0300

    refactor: adiciona prefixo /auth nas rotas de autenticação

--- src/main/java/br/com/user_service/controller/AuthenticationController.java
+++ src/main/java/br/com/user_service/controller/AuthenticationController.java
@@ -11,11 +11,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthenticationController {
 
     private final AuthenticationService service;
