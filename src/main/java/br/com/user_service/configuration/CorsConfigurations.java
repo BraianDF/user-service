74b12f350504ffd500cf7eff0852cbf48786cb5f
BraianDF <geraldo.bryan.vbnb@gmail.com>
2026-05-15 16:43:45 -0300

    fix(auth): trata usuário não encontrado no UserDetailsService

---
+++ src/main/java/br/com/user_service/configuration/CorsConfigurations.java
@@ -0,0 +1,7 @@
+package br.com.user_service.configuration;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Co
+public class CorsConfig implements WebMvcConfigurer {
+}
