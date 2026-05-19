5ac331e22d2ef5fb38a977f062c93941bbe25125
BraianDF <geraldo.bryan.vbnb@gmail.com>
2026-05-19 10:11:15 -0300

    refactor(cors): externaliza origens permitidas para variável de ambiente

--- src/main/java/br/com/user_service/configuration/CorsConfigurations.java
+++ src/main/java/br/com/user_service/configuration/CorsConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.user_service.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfigurations implements WebMvcConfigurer {
 
+    @Value("${cors.allowed-origin}")
+    private String allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*") // libera qualquer origem
+                .allowedOriginPatterns(allowedOrigins.split(","))
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
