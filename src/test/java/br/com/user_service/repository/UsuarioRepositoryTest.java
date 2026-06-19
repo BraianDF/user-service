7df579545166543928e0cbf3f8303690798ededc
BraianDF <geraldo.bryan.vbnb@gmail.com>
2026-06-19 11:13:03 -0300

    test: adiciona testes de integração para existsByEmail e existsByEmailAndPublicIdNot

--- src/test/java/br/com/user_service/repository/UsuarioRepositoryTest.java
+++ src/test/java/br/com/user_service/repository/UsuarioRepositoryTest.java
@@ -10,9 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
 import java.util.Set;
+import java.util.UUID;
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -53,8 +56,108 @@ class UsuarioRepositoryTest {
         assertThat(usuario.isEmpty()).isTrue();
     }
 
+    @Test
+    @DisplayName("Deve retornar true quando o e-mail existir")
+    void existsByEmailSucesso() {
+        // Arrange (preparação)
+        String email = "joao123@email.com";
+        UsuarioCadastrarRequestDTO dto = new UsuarioCadastrarRequestDTO(email, "123456", Set.of(Role.USER));
+        criarUsuario(dto);
+
+        // Act (ação)
+        boolean existe = usuarioRepository.existsByEmail(email);
+
+        // Assert (validação)
+        assertTrue(existe);
+    }
+
+    @Test
+    @DisplayName("Deve retornar false quando o e-mail não existir")
+    void existsByEmailErro() {
+        // Arrange (preparação)
+        String email = "naoexiste@email.com";
+
+        // Act (ação)
+        boolean existe = usuarioRepository.existsByEmail(email);
+
+        // Assert (validação)
+        assertFalse(existe);
+    }
+
+    @Test
+    @DisplayName("Deve retornar true quando existir outro usuário com o mesmo e-mail")
+    void existsByEmailAndPublicIdNotSucesso() {
+        // Arrange (preparação)
+        String email = "joao123@email.com";
+        UUID publicId = UUID.randomUUID();
+        UUID outroId = UUID.randomUUID();
+        UsuarioCadastrarRequestDTO dto = new UsuarioCadastrarRequestDTO(email, "123456", Set.of(Role.USER));
+        criarUsuarioPublicId(dto, publicId);
+
+        // Act (ação)
+        boolean existe = usuarioRepository.existsByEmailAndPublicIdNot(email, outroId);
+
+        // Assert (validação)
+        assertTrue(existe);
+    }
+
+    @Test
+    @DisplayName("Deve retornar false quando o e-mail pertencer ao próprio usuário")
+    void existsByEmailAndPublicIdNotErro1() {
+        // Arrange (preparação)
+        String email = "joao123@email.com";
+        UUID publicId = UUID.randomUUID();
+        UsuarioCadastrarRequestDTO dto = new UsuarioCadastrarRequestDTO(email, "123456", Set.of(Role.USER));
+        criarUsuarioPublicId(dto, publicId);
+
+        // Act (ação)
+        boolean existe = usuarioRepository.existsByEmailAndPublicIdNot(email, publicId);
+
+        // Assert (validação)
+        assertFalse(existe);
+    }
+
+    @Test
+    @DisplayName("Deve retornar false quando existir usuário com outro e-mail")
+    void existsByEmailAndPublicIdNotErro2() {
+        // Arrange (preparação)
+        String email = "joao123@email.com";
+        String outroEmail = "naoexiste@email.com";
+        UUID publicId = UUID.randomUUID();
+        UsuarioCadastrarRequestDTO dto = new UsuarioCadastrarRequestDTO(email, "123456", Set.of(Role.USER));
+        criarUsuarioPublicId(dto, publicId);
+
+        // Act (ação)
+        boolean existe = usuarioRepository.existsByEmailAndPublicIdNot(outroEmail, publicId);
+
+        // Assert (validação)
+        assertFalse(existe);
+    }
+
+    @Test
+    @DisplayName("Deve retornar false quando o e-mail não existir")
+    void existsByEmailAndPublicIdNotErro3() {
+        // Arrange (preparação)
+        String email = "naoexiste@email.com";
+        UUID publicId = UUID.randomUUID();
+
+        // Act (ação)
+        boolean existe = usuarioRepository.existsByEmailAndPublicIdNot(email, publicId);
+
+        // Assert (validação)
+        assertFalse(existe);
+    }
+
     private Usuario criarUsuario(UsuarioCadastrarRequestDTO dto) {
         Usuario usuario = new Usuario(dto.email(), dto.senha(), dto.roles());
+        entityManager.persist(usuario);
+        entityManager.flush();
+        return usuario;
+    }
+
+    private Usuario criarUsuarioPublicId(UsuarioCadastrarRequestDTO dto, UUID publicId) {
+        Usuario usuario = new Usuario(dto.email(), dto.senha(), dto.roles());
+        usuario.setPublicId(publicId);
         entityManager.persist(usuario);
         entityManager.flush();
         return usuario;
